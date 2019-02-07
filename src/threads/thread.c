79534d2341b6c56ac0e8e67ef120a3157cde28a8
Daksh Shah <daksh17336@iiitd.ac.in>
2019-02-07 19:12:42 +0530

    trying to get rid of the possible infinte loop

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -495,9 +495,12 @@ next_thread_to_run (void)
     return idle_thread;
   else{
     struct thread * next = list_entry (list_pop_front (&ready_list), struct thread, elem);
+    struct thread *start = next;
     while(next-> minStartTime < timer_ticks ()){//possibility of infinte loop?
       list_push_back (&ready_list, &next->elem);
       next = list_entry (list_pop_front (&ready_list), struct thread, elem);
+      if(next == start)
+        return idle_thread;
     }
     return next;
   }
