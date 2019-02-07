cbbdf30740d50a425e4645c110a484080f75af5c
Daksh Shah <daksh17336@iiitd.ac.in>
2019-02-07 19:32:04 +0530

    Initializing the value of minTimeStart

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -464,6 +464,7 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  initial_thread->minStartTime=0;
   t->magic = THREAD_MAGIC;
 
   old_level = intr_disable ();
