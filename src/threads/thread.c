a4785087a5f0f2114fdddbea4cb941d1ff6cf1a8
Daksh Shah <daksh17336@iiitd.ac.in>
2019-02-09 02:41:15 +0530

    Ensuring 'A thread may raise or lower its own priority at any time, but lowering its priority such that it no longer has the highest priority must cause it to immediately yield the CPU.' cleaning that up just a lil bit

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -371,10 +371,8 @@ thread_set_priority (int new_priority)
 
   if (!list_empty (&ready_list)){
     struct thread * top_ready = list_entry(list_begin(&ready_list),struct thread, elem);
-    if(thread_get_priority() < top_ready->priority){
-      // printf("DAKSH: Yielding the thread with %d priority to the thread with %d prior\n", thread_get_priority(),top_ready->priority);
-      thread_yield();
-    }  
+    if(thread_get_priority() < top_ready->priority)
+      thread_yield();  
   }
 }
 
